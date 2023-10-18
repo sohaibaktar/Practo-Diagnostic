b502b856c93250cb350ed094a32cbec5c240af89
Sohaib Aktar Gazi <aktar19sohaib@gmail.com>
2023-10-18 22:50:48 +0530

    initial commit

---
+++ src/main/java/com/doctors/DiagnosticOrderApplication.java
@@ -0,0 +1,13 @@
+package com.doctors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctors/DiagnosticOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
